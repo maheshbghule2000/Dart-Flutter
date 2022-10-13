eadf840d4b61ceaa9327501858d230669a760a70
mahesh ghule <maheshbghule2000@gmail.com>
2022-10-13 14:49:53 +0530

    factorial number

---
+++ fact.dart
@@ -0,0 +1,11 @@
+// while loop
+void main() { 
+   var num = 5; 
+   var factorial = 1; 
+   
+   while(num >=1) { 
+      factorial = factorial * num; 
+      num--; 
+   } 
+   print("The factorial  is ${factorial}"); 
+}  
