2c4c2068e69956d4d0ceb53ed19c86cf4f3d2dda
mahesh ghule <maheshbghule2000@gmail.com>
2022-10-19 15:56:58 +0530

    add

---
+++ day11/add.dart
@@ -0,0 +1,9 @@
+double addnumber(double num1, double num2) {
+  return num1 + num2;
+}
+void main() {
+  double firstresult = addnumber(1, 3.0);
+      firstresult = addnumber(1, 1);
+  print(firstresult + 1);
+  print('hello..!');
+}
